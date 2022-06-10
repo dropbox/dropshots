7abf69383ccf3347c904eb5b47ced4c9f54a62f4
Ryan Harter <rharter@dropbox.com>
2022-06-10 16:18:50 -0500

    adds test for plugin ordering.

--- dropshots-gradle-plugin/src/test/kotlin/com/dropbox/dropshots/DropshotsPluginTest.kt
+++ dropshots-gradle-plugin/src/test/kotlin/com/dropbox/dropshots/DropshotsPluginTest.kt
@@ -1,10 +1,11 @@
 package com.dropbox.dropshots
 
+import com.google.common.truth.Truth.assertThat
 import java.io.File
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
+import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 
 class DropshotsPluginTest {
@@ -23,6 +24,17 @@ class DropshotsPluginTest {
     gradleRunner
       .withArguments(":module:tasks", "--configuration-cache", "--stacktrace")
       .runFixture(fixtureRoot) { build() }
+  }
+
+  @Test
+  fun `applies to library plugins applied after plugin`() {
+    val fixtureRoot = File("src/test/projects/configuration-cache-compatible")
+
+    val result = gradleRunner
+      .withArguments(":module:tasks", "--configuration-cache", "--stacktrace")
+      .runFixture(fixtureRoot) { build() }
+    assertThat(result.output).contains("updateDebugScreenshots")
+    assertThat(result.output).contains("pullDebugScreenshots")
   }
 
   private fun GradleRunner.runFixture(
