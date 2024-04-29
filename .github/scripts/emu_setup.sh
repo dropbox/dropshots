f2ea4d1a139969c0f7fb5d9e66c64c5a6eeaeea4
Ryan Harter <rharter@dropbox.com>
2024-04-29 15:54:55 -0500

    Fixes some emulator setup script commands

--- .github/scripts/emu_setup.sh
+++ .github/scripts/emu_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 adb shell settings put global sysui_demo_allowed 1
-adb shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm\n1234
+adb shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm 1234
+adb shell am broadcast -a com.android.systemui.demo -e command battery -e level 100 -e plugged false
 adb shell am broadcast -a com.android.systemui.demo -e command notifications -e visible false
 adb shell am broadcast -a com.android.systemui.demo -e command network -e wifi show -e level 4
 adb shell cmd overlay enable com.android.internal.systemui.navbar.gestural
