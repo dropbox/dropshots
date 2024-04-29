0f154c7c382f67fb07c770f370ca864cac87db53
Ryan Harter <rharter@dropbox.com>
2024-04-29 15:01:17 -0500

    Adds some emulator config.

---
+++ .github/scripts/emu_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+adb shell settings put global sysui_demo_allowed 1
+adb shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm\n1234
+adb shell am broadcast -a com.android.systemui.demo -e command notifications -e visible false
+adb shell am broadcast -a com.android.systemui.demo -e command network -e wifi show -e level 4
+adb shell cmd overlay enable com.android.internal.systemui.navbar.gestural
