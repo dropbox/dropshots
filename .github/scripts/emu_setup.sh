dc2fe638d78fbdf3f5b6c6c2f0a7e9359a8d9e0f
Ryan Harter <rharter@dropbox.com>
2024-04-30 11:40:32 -0500

    Updates emulator setup script to ensure that demo mode is turned on.

--- .github/scripts/emu_setup.sh
+++ .github/scripts/emu_setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+adb wait-for-devices
 adb shell settings put global sysui_demo_allowed 1
+adb shell am broadcast -a com.android.systemui.demo -e command enter
 adb shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm 1234
-adb shell am broadcast -a com.android.systemui.demo -e command battery -e level 100 -e plugged false
-adb shell am broadcast -a com.android.systemui.demo -e command notifications -e visible false
+adb shell am broadcast -a com.android.systemui.demo -e command battery -e plugged false
+adb shell am broadcast -a com.android.systemui.demo -e command battery -e level 100
 adb shell am broadcast -a com.android.systemui.demo -e command network -e wifi show -e level 4
+adb shell am broadcast -a com.android.systemui.demo -e command network -e mobile show -e datatype none -e level 4
+adb shell am broadcast -a com.android.systemui.demo -e command notifications -e visible false
 adb shell cmd overlay enable com.android.internal.systemui.navbar.gestural
