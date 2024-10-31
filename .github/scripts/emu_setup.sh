3a55b21972fc6bcbe224471c3ec467cd992fefd3
Ryan Harter <rharter@dropbox.com>
2024-10-31 11:33:09 -0500

    removes unused build script.

--- .github/scripts/emu_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-adb wait-for-devices
-
-echo "Cleaning up old emulator data"
-adb uninstall com.dropbox.dropshots.test || true
-adb shell rm -rf /storage/emulated/0/Download/screenshots || true
