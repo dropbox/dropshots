09fbf9e5e7afc4fa8ba95e7e2c34194db4af2cee
Ryan Harter <ryanjharter@gmail.com>
2024-11-07 14:49:04 -0600

    Fixes plugin tests for versions (#71)
    * Updates plugin tests to fix execution when dropshots version hasn't
    been released yet.
    
    * Updates dependency versions.
    
    * Fixes incorrect snapshot in version name.

--- dropshots-gradle-plugin/src/test/kotlin/com/dropbox/dropshots/DropshotsPluginTest.kt
+++ dropshots-gradle-plugin/src/test/kotlin/com/dropbox/dropshots/DropshotsPluginTest.kt
@@ -11,56 +11,66 @@ import org.junit.rules.TemporaryFolder
 
 class DropshotsPluginTest {
 
-  private val agpVersion = "8.7.1"
   @get:Rule val tmpFolder = TemporaryFolder()
 
-  private lateinit var projectDir: File
-  private lateinit var settingsFile: File
   private lateinit var buildFile: File
   private lateinit var gradleRunner: GradleRunner
 
   @Before
   fun setup() {
+    val localMavenRepo = File("../build/localMaven").absolutePath
+    val versionsFilePath = File("../gradle/libs.versions.toml").absolutePath
+
     // Setup project directory
-    projectDir = tmpFolder.newFolder().apply { mkdir() }
-    File(projectDir, "gradle.properties").writeText("android.useAndroidX=true")
-    // language=groovy
-    settingsFile = File(projectDir, "settings.gradle").apply {
-      writeText(
-        """
-          pluginManagement {
-            repositories {
-              gradlePluginPortal()
-              mavenCentral()
-              google()
-            }
-          }
-        """.trimIndent()
-      )
-    }
-    // language=groovy
-    buildFile = File(projectDir, "build.gradle").apply {
-      writeText(
-        """
-          plugins {
-            id("com.android.library") version "$agpVersion"
-            id("com.dropbox.dropshots")
-          }
+    val projectDir = tmpFolder.newFolder().apply { mkdir() }
+    projectDir.resolve("gradle.properties").writeText("android.useAndroidX=true")
 
-          android {
-            namespace = "com.dropbox.dropshots.test.library"
-            compileSdk = 35
-
-            defaultConfig.minSdk = 24
-          }
-
+    projectDir.resolve("settings.gradle").writeText(
+      // language=groovy
+      """
+        pluginManagement {
           repositories {
+            gradlePluginPortal()
             mavenCentral()
             google()
           }
-        """.trimIndent()
-      )
-    }
+        }
+
+        dependencyResolutionManagement {
+          versionCatalogs {
+            libs {
+              from(files("$versionsFilePath"))
+            }
+          }
+
+          repositories {
+            maven {
+              url("$localMavenRepo")
+            }
+            mavenCentral()
+            google()
+          }
+        }
+      """.trimIndent()
+    )
+
+    buildFile = projectDir.resolve("build.gradle")
+    buildFile.writeText(
+      // language=groovy
+      """
+        plugins {
+          alias(libs.plugins.android.library)
+          id("com.dropbox.dropshots")
+        }
+
+        android {
+          namespace = "com.dropbox.dropshots.test.library"
+          compileSdk = 35
+
+          defaultConfig.minSdk = 24
+        }
+      """.trimIndent()
+    )
 
     gradleRunner = GradleRunner.create()
       .withPluginClasspath()
@@ -69,26 +79,30 @@ class DropshotsPluginTest {
 
   @Test
   fun configurationCache() {
-    gradleRunner
+    // first build
+    gradleRunner.withArguments("tasks", "--configuration-cache").build()
+
+    // Cached build
+    val result = gradleRunner
       .withArguments("tasks", "--configuration-cache", "--stacktrace")
       .build()
+    assertThat(result.output).contains("Reusing configuration cache.")
   }
 
   @Test
   fun `applies to library plugins applied after plugin`() {
     val result = gradleRunner
       .withBuildScript(
-        // language = groovy
+        // language=groovy
         """
           plugins {
             id("com.dropbox.dropshots")
-            id("com.android.library") version "$agpVersion"
+            alias(libs.plugins.android.library)
           }
 
           android {
             namespace = "com.dropbox.dropshots.test.library"
             compileSdk = 35
-
             defaultConfig.minSdk = 24
           }
 
@@ -98,7 +112,7 @@ class DropshotsPluginTest {
           }
         """.trimIndent()
       )
-      .withArguments("tasks", "--configuration-cache", "--stacktrace")
+      .withArguments("tasks")
       .build()
     assertThat(result.output).contains("recordDebugAndroidTestScreenshots")
     assertThat(result.output).contains("pullDebugAndroidTestScreenshots")
@@ -107,7 +121,7 @@ class DropshotsPluginTest {
   @Test
   fun `executes marker file push only when record task is run`() {
     val result = gradleRunner
-      .withArguments("recordDebugAndroidTestScreenshots", "--stacktrace")
+      .withArguments("recordDebugAndroidTestScreenshots")
       .build()
     with(result.task(":pushDebugAndroidTestScreenshotMarkerFile")) {
       assertThat(this).isNotNull()
@@ -118,7 +132,7 @@ class DropshotsPluginTest {
   @Test
   fun `skips marker file push only when record task is run`() {
     val result = gradleRunner
-      .withArguments("connectedDebugAndroidTest", "--stacktrace")
+      .withArguments("connectedDebugAndroidTest")
       .build()
     with(result.task(":pushDebugAndroidTestScreenshotMarkerFile")) {
       assertThat(this).isNotNull()
